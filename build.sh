e4555e165cc03282086ca50ea8b52e8def190b8d
dabolai <dabo@jointoenjoy.com>
2026-07-20 19:41:09 +0800

    p21 博報堂改為整體佔比呈現；加入擋爬蟲設定與 /2026 部署腳本

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# 產生部署資料夾：內容放在 /2026，並加上擋爬蟲設定
+set -e
+rm -rf dist && mkdir -p dist/2026
+cp index.html dist/2026/index.html
+printf 'User-agent: *\nDisallow: /\n' > dist/robots.txt
+printf '/*\n  X-Robots-Tag: noindex, nofollow, noarchive, nosnippet\n  Referrer-Policy: no-referrer\n' > dist/_headers
+printf '/  /2026/  302\n' > dist/_redirects
+echo "dist 已產生"
